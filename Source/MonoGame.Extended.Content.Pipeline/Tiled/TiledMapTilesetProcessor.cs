64d5c5b01a315294cd0866da1608170c1358cdbf
dookv <dookv@users.noreply.github.com>
2024-02-28 22:36:40 +0100

    added the tiledmap processor fixes from monogame extended develop branch to this fork

--- Source/MonoGame.Extended.Content.Pipeline/Tiled/TiledMapTilesetProcessor.cs
+++ Source/MonoGame.Extended.Content.Pipeline/Tiled/TiledMapTilesetProcessor.cs
@@ -15,24 +15,19 @@ namespace MonoGame.Extended.Content.Pipeline.Tiled
 
 			    ContentLogger.Logger = context.Logger;
 				ContentLogger.Log($"Processing tileset '{tileset.Name}'");
-                
-				// Build the Texture2D asset and load it as it will be saved as part of this tileset file.
-			    //var externalReference = new ExternalReference<Texture2DContent>(tileset.Image.Source);
-			    var parameters = new OpaqueDataDictionary
-			    {
-			        { "ColorKeyColor", tileset.Image.TransparentColor },
-			        { "ColorKeyEnabled", true }
-			    };
-			    //tileset.Image.ContentRef = context.BuildAsset<Texture2DContent, Texture2DContent>(externalReference, "", parameters, "", "");
-			    contentItem.BuildExternalReference<Texture2DContent>(context, tileset.Image.Source, parameters);
 
-				foreach (var tile in tileset.Tiles)
+                // Build the Texture2D asset and load it as it will be saved as part of this tileset file.
+                if (tileset.Image != null)
+                    contentItem.BuildExternalReference<Texture2DContent>(context, tileset.Image);
+                foreach (var tile in tileset.Tiles)
 				{
 				    foreach (var obj in tile.Objects)
 				    {
 				        TiledMapContentHelper.Process(obj, context);
 				    }
-				}
+                    if (tile.Image != null)
+                        contentItem.BuildExternalReference<Texture2DContent>(context, tile.Image);
+                }
 
 			    ContentLogger.Log($"Processed tileset '{tileset.Name}'");
 
